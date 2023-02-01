ce27a6d003ae82b0765e15d42e1a27a75a67ff09
thanatcha sureeyapun <thanatcha4714@gmail.com>
2023-02-01 10:13:31 +0700

    Lab09

---
+++ lib/auth_service.dart
@@ -0,0 +1,30 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+
+class GoogleAuthService {
+  static GoogleSignIn _googleSignin = GoogleSignIn();
+  Future<void> signinbygoogle() async {
+    final GoogleSignInAccount? googleUser = await _googleSignin.signIn();
+    final GoogleSignInAuthentication? googleAuth =
+        await googleUser?.authentication;
+
+    print(googleUser?.displayName);
+    print(googleAuth?.accessToken);
+    print(googleAuth?.idToken);
+
+    final credential = GoogleAuthProvider.credential(
+      accessToken: googleAuth?.accessToken,
+      idToken: googleAuth?.idToken,
+    );
+
+    UserCredential userCredential =
+        await FirebaseAuth.instance.signInWithCredential(credential);
+    print(userCredential.user);
+  }
+
+  Future<void> googlesingout() async {
+    await FirebaseAuth.instance.signOut().then((value) {
+      _googleSignin.signOut();
+    });
+  }
+}
